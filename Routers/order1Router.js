e37ecdbbec790605e2507ed210cf07b6e0cc241a
Epic-Apps-Hub <m4hmoudatef@gmail.com>
2021-09-24 15:58:42 +0200

    gf

--- Routers/order1Router.js
+++ Routers/order1Router.js
@@ -12,6 +12,7 @@ const {
 } = require('../Controllers/order1Controller')
 Order1Router.route('/add').post(addOrder)
 
+
 Order1Router.route('/getUTP/:id').get(getUserToPerson)
 Order1Router.route('/getSTU/:id').get(getShopToUser)
 Order1Router.route('/captainOrders').post(getCaptainOrders)
